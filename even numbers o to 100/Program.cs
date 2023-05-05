f8269f913cd95e6d73bdbf1e0393aa296524e68c
mightysodiq <muritalasodiqbab1@gmail.com>
2023-05-05 04:49:59 +0100

    Add project files.

---
+++ even numbers o to 100/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+
+namespace even
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            } 
+        }
+    }
+}
